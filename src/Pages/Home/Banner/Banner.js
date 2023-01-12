6c8e127d61f8355912b22eafb15492d02c57ae00
Mezan2002 <mezanurr009@gmail.com>
2023-01-13 02:59:47 +0600

    Banner done

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,11 +1,11 @@
 import React from "react";
-import { FaLocationArrow, FaSearch, FaSearchLocation } from "react-icons/fa";
+import { FaLocationArrow, FaSearch } from "react-icons/fa";
 
 const Banner = () => {
   return (
-    <div className="bg-gradient-to-l from-[#E8EEFA] to-[#F2F5FC] pt-20">
-      <div className="grid grid-cols-2 min-h-[90vh] px-40 my-20">
-        <div>
+    <div className="bg-gradient-to-l from-[#E8EEFA] to-[#F2F5FC]">
+      <div className="flex items-center justify-between min-h-[90vh] px-40">
+        <div className="w-1/2">
           <div>
             <h2 className="uppercase text-4xl font-semibold">
               We Have a lots of jobs for you, Find Your Perfect Job Match
@@ -44,8 +44,128 @@ const Banner = () => {
               Popular Searches : Designer Developer Web IOS PHP Senior Engineer
             </h2>
           </div>
+          <div className="flex items-center">
+            <p className="text-xl font-medium">5k+ candidates</p>
+            <div className="avatar-group -space-x-6">
+              <div className="avatar">
+                <div className="w-12">
+                  <img src="https://placeimg.com/192/192/people" alt="" />
+                </div>
+              </div>
+              <div className="avatar">
+                <div className="w-12">
+                  <img src="https://placeimg.com/192/192/people" alt="" />
+                </div>
+              </div>
+              <div className="avatar">
+                <div className="w-12">
+                  <img src="https://placeimg.com/192/192/people" alt="" />
+                </div>
+              </div>
+              <div className="avatar placeholder">
+                <div className="w-12 bg-neutral-focus text-white">
+                  <span className="text-xl">+</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div></div>
+        <div className="w-1/2">
+          {/* <div className="rounded-full w-[550px] h-[550px] border-dashed border-2 border-indigo-200 absolute top-9 right-[79px] z-0"></div> */}
+          <div className="flex justify-center">
+            <img
+              src="https://i.ibb.co/3SNzbqs/slider112-removebg-preview-1.png"
+              alt=""
+              className="mt-[-90px] w-full"
+            />
+          </div>
+          {/* <div>
+            <div className="">
+              <div
+                className="bg-white w-24 h-24 rounded-full flex items-center justify-center absolute 
+               top-[-40px] right-0 animation"
+              >
+                <img
+                  src="https://i.ibb.co/BzR6sYh/sketch.png"
+                  className="w-16"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="">
+              <div
+                className="bg-white w-20 h-20 rounded-full flex items-center justify-center absolute 
+               top-[-20px] left-80 animation"
+              >
+                <img
+                  src="https://i.ibb.co/44whybs/html-5.png"
+                  className="w-12"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="">
+              <div
+                className="bg-white w-20 h-20 rounded-full flex items-center justify-center absolute 
+               right-[-40px] bottom-0 animation"
+              >
+                <img
+                  src="https://i.ibb.co/1JsKH6X/js.png"
+                  className="w-12"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="">
+              <div
+                className="bg-white w-20 h-20 rounded-full flex items-center justify-center absolute 
+               bottom-32 left-56 animation"
+              >
+                <img
+                  src="https://i.ibb.co/JsKWcfM/css-3.png"
+                  className="w-12"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="">
+              <div
+                className="bg-white w-20 h-20 rounded-full flex items-center justify-center absolute 
+        bottom-[-97px] left-[470px] animation"
+              >
+                <img
+                  src="https://i.ibb.co/JpvfzBr/photoshop.png"
+                  className="w-14"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="">
+              <div
+                className="bg-white w-16 h-16 rounded-full flex items-center justify-center absolute 
+        top-[-30px] left-[500px] animation"
+              >
+                <img
+                  src="https://i.ibb.co/VHfW5vN/illustrator.png"
+                  className="w-10"
+                  alt=""
+                />
+              </div>
+            </div>
+            <div className="">
+              <div
+                className="bg-white w-16 h-16 rounded-full flex items-center justify-center absolute 
+        top-48 right-[-30px] animation"
+              >
+                <img
+                  src="https://i.ibb.co/ZH0YWYT/figma.png"
+                  className="w-10"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div> */}
+        </div>
       </div>
     </div>
   );
